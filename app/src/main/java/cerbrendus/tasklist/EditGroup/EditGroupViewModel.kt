e74e2a5b95bafcab30cf766b5068eda8fbbd6a84
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2019-05-07 13:10:48 +0200

    EditViewModel (super class) took over some functionality form the group editing VM

--- app/src/main/java/cerbrendus/tasklist/EditGroup/EditGroupViewModel.kt
+++ app/src/main/java/cerbrendus/tasklist/EditGroup/EditGroupViewModel.kt
@@ -3,28 +3,25 @@ package cerbrendus.tasklist.EditGroup
 import android.app.Application
 import android.content.Intent
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProviders
-import cerbrendus.tasklist.Database.ItemRepository
+import cerbrendus.tasklist.Classes.EditViewModel
 import cerbrendus.tasklist.EditTaskItem.TYPE_ADD
-import cerbrendus.tasklist.EditTaskItem.TYPE_INTENT_KEY
-import cerbrendus.tasklist.EditTaskItem.TYPE_UPDATE
 import cerbrendus.tasklist.EditTaskItem.TYPE_VIEW
 import cerbrendus.tasklist.dataClasses.Group
 import cerbrendus.tasklist.dataClasses.TaskItem
 
 //Created by Brendan on 30-12-2018.
-class EditGroupViewModel(application: Application) : AndroidViewModel(application) {
-    private val itemRepo = ItemRepository.create(application)
+class EditGroupViewModel(application: Application) : EditViewModel(application) {
+    //private val itemRepo = ItemRepository.create(application)
 
     fun createGroup(group: Group) {itemRepo.createGroup(group)}
     fun updateGroup(group: Group) {itemRepo.updateGroup(group)}
     fun deleteGroup(group: Group) {itemRepo.deleteGroup(group)}
 
-    var editType : MutableLiveData<Int> = MutableLiveData()
-    var openedAsView = false
-    lateinit var intent : Intent
+    //var editType : MutableLiveData<Int> = MutableLiveData()
+    //var openedAsView = false
+    //lateinit var intent : Intent
     var currentGroup : MutableLiveData<Group> = MutableLiveData()
     lateinit var itemList : List<TaskItem>
 
@@ -40,22 +37,22 @@ class EditGroupViewModel(application: Application) : AndroidViewModel(applicatio
             else vm!!
     }
 
-    fun configure(_intent : Intent) : Boolean {
-        /*  Configures the ViewModel according to the intent.
-        *   Returns 'true' if it was successful, otherwise 'false' */
-
-        // Set the intent
+    /*  Configures the ViewModel according to the intent.
+    *   Returns 'true' if it was successful, otherwise 'false' */
+    override fun configure(_intent : Intent) : Boolean {
+        super.configure(_intent)
+        /*// Set the intent
         this.intent = _intent
 
         // Get the editType value
-        editType.value = intent.getIntExtra(TYPE_INTENT_KEY, TYPE_ADD)
+        editType.value = intent.getIntExtra(TYPE_INTENT_KEY, TYPE_ADD)*/
 
         // If not passed, currentGroup set to empty item, if it should be passed return false
         currentGroup.value = intent.getParcelableExtra<Group>(GROUP_KEY) ?:
                 if (editType.value == TYPE_ADD) Group() else return false
 
-        //Set check-value for Activity opened in view mode
-        openedAsView = (editType.value == TYPE_VIEW)
+        /*//Set check-value for Activity opened in view mode
+        openedAsView = (editType.value == TYPE_VIEW)*/
 
         // Get itemList from intent or from repo or empty
         itemList = intent.getParcelableArrayListExtra(ITEM_LIST_KEY) ?: itemRepo.getAll().value.orEmpty()
@@ -64,22 +61,13 @@ class EditGroupViewModel(application: Application) : AndroidViewModel(applicatio
         return true
     }
 
-    fun save() : Boolean {
-        return when(editType.value){
-            TYPE_ADD -> handleItemAdded()
-            TYPE_UPDATE -> handleItemUpdated()
-            else -> false
-        }
-    }
-
-    //Handle different  edit actions (update, add, delete)
-    private fun handleItemUpdated() : Boolean {
+    override fun handleUpdated(): Boolean {
         updateGroup(currentGroup.value!!)
         editType.value = TYPE_VIEW
         return true
     }
 
-    private fun handleItemAdded() : Boolean {
+    override fun handleAdded() : Boolean {
         createGroup(currentGroup.value!!)
         return true
     }
