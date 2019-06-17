0fc1a25b7702df23981ed31ea489d319fdab6e94
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2019-06-17 23:10:17 +0200

    Added SublistAdapter

---
+++ app/src/main/java/cerbrendus/tasklist/BaseClasses/SublistAdapter.kt
@@ -0,0 +1,60 @@
+package cerbrendus.tasklist.BaseClasses
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import cerbrendus.tasklist.R
+import cerbrendus.tasklist.dataClasses.TaskItem
+
+const val VIEWTYPE_ITEM = 0
+const val VIEWTYPE_ADD = 1
+
+class SublistAdapter(private val itemList: List<TaskItem>, val context: FragmentActivity, val displayAdd : Boolean) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when(viewType) {
+            VIEWTYPE_ITEM -> SublistViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.recyclerview_item, parent, false)
+            )
+            VIEWTYPE_ADD -> AddButtonViewHolder(LayoutInflater.from(parent.context).inflate(
+                R.layout.recyclerview_item, parent, false)
+            )
+            else ->  SublistViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.recyclerview_item, parent, false)
+            )
+        }
+
+    override fun getItemCount(): Int = itemList.size + (if (displayAdd) 1 else 0)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(displayAdd && position==itemList.size){
+            // set onClickListener
+        }
+        else {
+            val vh = holder as SublistViewHolder
+            vh.title?.setText(itemList[position].title)
+            vh.check?.isChecked = itemList[position].checked
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int = if(displayAdd && position==itemList.size) VIEWTYPE_ADD else VIEWTYPE_ITEM
+
+}
+
+class SublistViewHolder(attributeView: View) : RecyclerView.ViewHolder(attributeView) {
+    val view = attributeView
+    val title = view.findViewById<TextView?>(R.id.task_title)
+    val check = view.findViewById<CheckBox?>(R.id.attribute_icon)
+}
+
+class AddButtonViewHolder(attributeView: View) : RecyclerView.ViewHolder(attributeView) {
+    val view = attributeView
+    val title = view.findViewById<TextView?>(R.id.task_title)
+    val check = view.findViewById<CheckBox?>(R.id.attribute_icon)
+}
