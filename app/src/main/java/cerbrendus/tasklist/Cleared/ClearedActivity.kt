09f2dcc6023bcb496dc9aa19d33885a0fa7d3ce9
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2019-08-03 21:08:15 +0200

    Added setting the drawer up in the ClearedActivity

--- app/src/main/java/cerbrendus/tasklist/Cleared/ClearedActivity.kt
+++ app/src/main/java/cerbrendus/tasklist/Cleared/ClearedActivity.kt
@@ -1,13 +1,18 @@
 package cerbrendus.tasklist.Cleared
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import cerbrendus.tasklist.Main.MainActivity
 import cerbrendus.tasklist.R
 import com.google.android.material.navigation.NavigationView
 import kotlinx.coroutines.launch
@@ -21,6 +26,30 @@ class ClearedActivity : AppCompatActivity() {
         val vm = ClearedViewModel.create(this)
         val toolbar: Toolbar = findViewById(R.id.toolbar_cleared_activity)
         setSupportActionBar(toolbar)
+
+        //Set the drawer
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home_drawer_item -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.groups_drawer_item -> {
+                }
+                R.id.cleared_drawer_item -> {}
+                R.id.settings_drawer_item -> {
+                }
+            }
+            drawer.closeDrawer(GravityCompat.START)
+            true
+        }
 
         //Get RecyclerView handle
         val recyclerView: RecyclerView = findViewById(R.id.cleared_recyclerview)
