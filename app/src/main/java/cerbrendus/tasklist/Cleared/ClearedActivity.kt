21b8d0ad41ff0ff41d775e95ff1aa1c08b3587fb
cerbrendus <34724641+cerbrendus@users.noreply.github.com>
2019-08-03 15:26:59 +0200

    Implemented dragHandler for swiping items back to be uncleared

--- app/src/main/java/cerbrendus/tasklist/Cleared/ClearedActivity.kt
+++ app/src/main/java/cerbrendus/tasklist/Cleared/ClearedActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cerbrendus.tasklist.R
+import kotlinx.coroutines.launch
 
 class ClearedActivity : AppCompatActivity() {
 
@@ -26,9 +28,25 @@ class ClearedActivity : AppCompatActivity() {
         recyclerView.addItemDecoration(itemDecor)
 
         //Set RecyclerView adapter
-        val adapter = ClearedItemAdapter(vm.allClearedItems.value.orEmpty(),vm.groupColorMap.value.orEmpty(), this)
+        val adapter = ClearedItemAdapter(vm.allClearedItems.value.orEmpty(), vm.groupColorMap.value.orEmpty(), this)
         recyclerView.adapter = adapter
         vm.allClearedItems.observe(this, Observer { adapter.setItems(it) })
         vm.groupColorMap.observe(this, Observer { adapter.setColorMap(it) })
+
+        // Set drag handler
+        val dragHandler = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(r: RecyclerView, v: RecyclerView.ViewHolder, t: RecyclerView.ViewHolder): Boolean =
+                false
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val pos = viewHolder.adapterPosition
+                vm.scope.launch {
+                    adapter.removeItem(vm.allClearedItems.value.orEmpty().toMutableList().apply { removeAt(pos) }, pos)
+                    vm.unclearItem(pos)
+                }
+            }
+        }
+        val dragHelper = ItemTouchHelper(dragHandler)
+        dragHelper.attachToRecyclerView(recyclerView)
     }
 }
--- app/src/main/java/cerbrendus/tasklist/Cleared/ClearedViewModel.kt
+++ app/src/main/java/cerbrendus/tasklist/Cleared/ClearedViewModel.kt
@@ -6,12 +6,20 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModelProviders
 import cerbrendus.tasklist.Database.ItemRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 class ClearedViewModel(application: Application) : AndroidViewModel(application) {
     private val itemRepo = ItemRepository.create(application)
     val allClearedItems = itemRepo.getAllCleared()
     val groupColorMap = Transformations.map(itemRepo.getGroupList()) {
         it.map { group -> Pair(group.id!!, group.color) }.toMap()
+    }
+    val scope = CoroutineScope(Dispatchers.IO)
+
+    suspend fun unclearItem(pos: Int) {
+        val item = allClearedItems.value.orEmpty()[pos]
+        itemRepo.update(item.apply { cleared = false; clearedId = null })
     }
 
 
